cf8c454b1966c9ba43499c7bd3772ed0b6d5c082
yessicafv23 <2321yesica@gmail.com>
2022-08-20 22:26:51 -0500

    Primer Versión PY Market

---
+++ src/main/java/com/yf/market/YfMarketApplication.java
@@ -0,0 +1,13 @@
+package com.yf.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YfMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YfMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yf/market/YfMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yf.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YfMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
